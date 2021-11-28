de3917efbf9d68d3bd5b4b0cb06469ea9bac43ea
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-11-28 17:13:18 +0100

    Using Constructor in class

--- main.cpp
+++ main.cpp
@@ -3,31 +3,35 @@ using std::string;
 using std::cout;
 using std::endl;
 
-
+// Create class
 class Employee {
+// make class public
 public:
     string Name;
     string Company;
     int Age;
 
+// Create a function in the class
     void IntroduceSelf(){
         cout  <<"Name - " << Name << endl;
         cout  <<"Company - " << Company << endl;
         cout  <<"Age - " << Age << endl;
     }
+
+// Create a Constructor
+    Employee(string name, string company, int age){
+        Name = name;
+        Company = company;
+        Age = age;
+    }
+
 };
 
 int main() {
-    Employee employee1;
-    employee1.Name = "Ayuba";
-    employee1.Company = "MonnieTech";
-    employee1.Age = 30;
+    Employee employee1 = Employee("Ayuba", "MonnieTech", 30);
     employee1.IntroduceSelf();
 
-    Employee employee2;
-    employee2.Name = "Tahiru";
-    employee2.Company = "MonnieTechNow";
-    employee2.Age = 35;
+    Employee employee2  = Employee("Tahiru", "MonnieTechNow", 35);
     employee2.IntroduceSelf();
 
 
